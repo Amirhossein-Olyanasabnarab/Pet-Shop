d764d870bfafd4fa2e3914bdfd7d671cef1e863a
amirholya01 <amirholya01@gmail.com>
2024-12-03 10:39:56 +0100

    DBContext

---
+++ Src/Pet_Shop,Infrastructure/Pet_Shop.Persistence/Contexts/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pet_Shop.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet_Shop.Persistence.Contexts
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext()
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> UserInRoles { get; set; }
+    }
+}
